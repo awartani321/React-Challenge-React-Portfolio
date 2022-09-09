61e9e2cbedc6aaecd1b0883055a03013b82b6a8e
Adam Awartani <adamawartani321@gmail.com>
2022-09-08 21:47:54 -0400

    add resume page

---
+++ src/components/Resume.js
@@ -0,0 +1,15 @@
+import './Resume.css';
+
+function Resume() {
+    return (
+        <div className="resume-container">
+            <h2>Resume: &nbsp;&nbsp; <a className="button primary" target="_blank" href="https://writing.colostate.edu/guides/documents/resume/functionalsample.pdf">Download</a></h2>            
+
+            <div class="skills">
+                <p><b>Skills: </b> React, NodeJS, Java, C/C++, C#, Android, MySQL, MongoDB </p>                
+            </div>
+        </div>
+    )
+}
+
+export default Resume;
